479c1c166eb547265847e8912db9e20db4edb0c3
zi6xuan <zi6xuan@gmail.com>
2021-05-26 22:19:23 +0900

    Update TouchableOpacity.js

--- components/ListRow/TouchableOpacity.js
+++ components/ListRow/TouchableOpacity.js
@@ -3,8 +3,8 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-import {Platform, TouchableWithoutFeedback, Animated, ViewPropTypes} from 'react-native';
-import Easing from 'react-native/Libraries/Animated/src/Easing';
+import {Platform, TouchableWithoutFeedback, Animated, ViewPropTypes,Easing} from 'react-native';
+// import Easing from 'react-native/Libraries/Animated/src/Easing';
 import flattenStyle from 'react-native/Libraries/StyleSheet/flattenStyle';
 
 if (Platform.constants.reactNativeVersion.major === 0 && Platform.constants.reactNativeVersion.minor < 62) {
