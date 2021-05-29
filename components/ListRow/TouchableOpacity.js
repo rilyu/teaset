7b0147c36d23b1f9d70d4a78c36edc286dc3ec41
unknown <282748212@qq.com>
2021-05-29 16:51:14 +0800

    新版本react-native中Easing的路径发生了变化，对其进行了相应修复

--- components/ListRow/TouchableOpacity.js
+++ components/ListRow/TouchableOpacity.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import {Platform, TouchableWithoutFeedback, Animated, ViewPropTypes} from 'react-native';
-import Easing from 'react-native/Libraries/Animated/src/Easing';
+import Easing from 'react-native/Libraries/Animated/Easing';
 import flattenStyle from 'react-native/Libraries/StyleSheet/flattenStyle';
 
 if (Platform.constants.reactNativeVersion.major === 0 && Platform.constants.reactNativeVersion.minor < 62) {
